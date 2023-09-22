b573cf3208ebb4a24f15223067910523790e574c
Victor Sarov <victor.sarov@rentalmatics.com>
2023-09-22 16:41:02 +0100

    all done

--- src/components/charts/TreemapChart/TreemapChart.tsx
+++ src/components/charts/TreemapChart/TreemapChart.tsx
@@ -7,28 +7,14 @@ import {
     Typography,
 } from '@mui/material';
 import { t } from 'src/translation/index';
-import {
-    RM_COLOR_BASE_BLUE_MIDDLE,
-    RM_COLOR_BASE_BLUE_LIGHT_20,
-    RM_COLOR_BASE_BLUE_LIGHT_30,
-    RM_COLOR_BASE_BLUE_LIGHT_50,
-    RM_COLOR_BASE_BLUE_LIGHT_80,
-    RM_COLOR_BASE_BLUE_DARK_20,
-    RM_COLOR_BASE_BLUE_DARK_30,
-    RM_COLOR_BASE_BLUE_DARK_50,
-    RM_COLOR_BASE_BLUE_DARK_80,
-    RM_COLOR_BASE_WHITE,
-    RM_TYPOGRAPHY_FONT_FAMILY_SANS,
-    RM_SIZE_FONT_LABEL_LARGE,
-} from 'src/design-tokens/tokens';
+
 import {
     destroyTreeMapCdnAndChart,
-    loadTheTreeMapCDNScript,
     useReformatDataForTreeMap,
+    useDrawTreeMapChart,
 } from './hooks';
 
 const TREEMAP_HEIGHT = '525px';
-const TREEMAP_FONT_SIZE = Number(RM_SIZE_FONT_LABEL_LARGE.split(/r/)[0]) * 16;
 
 const TreemapChartSkeleton: React.FC<SkeletonProps> = ({
     variant = 'rectangular',
@@ -61,120 +47,24 @@ const TreemapChart: React.FC<TreemapChartProps> = ({
     ...rest
 }) => {
     //
-    const chartRef = React.useRef<any>(null);
-    const [chartIsDrawn, setChartIsDrawn] = React.useState(false);
-    const [dummyReloadState, setDummyReloadState] = React.useState(0);
+    const [chartIsDrawn, setChartIsDrawn] = React.useState<boolean>(false);
+    const [dummyReloadState, setDummyReloadState] = React.useState<number>(0);
     const [chartNotLoadingError, setChartNotLoadingError] =
-        React.useState(false);
-
-    const colorArr = React.useMemo(
-        () => [
-            RM_COLOR_BASE_BLUE_DARK_80,
-            RM_COLOR_BASE_BLUE_DARK_50,
-            RM_COLOR_BASE_BLUE_DARK_30,
-            RM_COLOR_BASE_BLUE_DARK_20,
-            RM_COLOR_BASE_BLUE_MIDDLE,
-            RM_COLOR_BASE_BLUE_LIGHT_20,
-            RM_COLOR_BASE_BLUE_LIGHT_30,
-            RM_COLOR_BASE_BLUE_LIGHT_50,
-            RM_COLOR_BASE_BLUE_LIGHT_80,
-        ],
-        []
-    );
+        React.useState<boolean>(false);
 
     const reformatDataForTreeMap = useReformatDataForTreeMap(data);
 
-    React.useEffect(() => {
-        if (chartIsDrawn) {
-            return;
-        }
-
-        loadTheTreeMapCDNScript();
-
-        let WindowChart = (window as any).Chart;
-        let ctx = chartRef?.current?.getContext('2d');
-        let chartTreeMapLoadedIntoChartJs= (window as any).Chart.defaults.treemap;
-
-        // * Wait for Chart Treemap to LOAD into the Window object (it takes few re-renders)
-        if (!chartTreeMapLoadedIntoChartJs || !ctx) {
-            // * need to stop INFINITE loop in case Chart CDN does NOT load for some reason
-            if (dummyReloadState >= 100) {
-                setChartNotLoadingError(true);
-                setChartIsDrawn(true);
-            }
-
-            setDummyReloadState(dummyReloadState + 1);
-            return;
-        }
-
-        // * Draw the chart if all is good
-        (window as any).chart1 = new WindowChart(ctx, {
-            type: 'treemap',
-            data: {
-                datasets: [
-                    {
-                        label: chartLabel,
-                        tree: reformatDataForTreeMap,
-                        key: 'value',
-                        groups: ['label'],
-                        backgroundColor: function (ctx: any) {
-                            let item = ctx.dataset.data[ctx.dataIndex];
-
-                            if (!item) {
-                                return;
-                            }
-
-                            const idx = ctx.dataset.tree.indexOf(
-                                ctx.dataset.data[ctx.dataIndex]._data
-                                    .children[0]
-                            );
-
-                            return colorArr[idx];
-                        },
-                        fontColor: RM_COLOR_BASE_WHITE,
-                        fontFamily: RM_TYPOGRAPHY_FONT_FAMILY_SANS,
-                        fontSize: TREEMAP_FONT_SIZE,
-                        spacing: 1,
-                        borderWidth: 3,
-                    },
-                ],
-            },
-            options: {
-                onClick: (el: any, arr: any) =>
-                    console.log(
-                        'item index clicked',
-                        linksArray[arr[0]._index]
-                    ),
-                maintainAspectRatio: false,
-                legend: {
-                    display: false,
-                },
-                tooltips: {
-                    callbacks: {
-                        title: function (item: any, data: any) {
-                            return `Item index is: ${item[0].index}`;
-                        },
-                        label: function (item: any, data: any) {
-                            const dataset = data.datasets[item.datasetIndex];
-                            const dataItem = dataset.data[item.index];
-                            return dataItem.v;
-                        },
-                    },
-                },
-            },
-        });
-
-        setChartIsDrawn(true);
-    }, [
+    useDrawTreeMapChart(
         data,
         loading,
-        chartIsDrawn,
-        colorArr,
-        dummyReloadState,
+        chartLabel,
         reformatDataForTreeMap,
-        linksArray,
-        chartLabel
-    ]);
+        chartIsDrawn,
+        dummyReloadState,
+        setChartIsDrawn,
+        setChartNotLoadingError,
+        setDummyReloadState
+    );
 
     React.useEffect(() => {
         // * when we trigger the Select Dropdown, we need to destroy the OLD chart and recreate a new one, otherwise it bugs a bit
@@ -198,12 +88,7 @@ const TreemapChart: React.FC<TreemapChartProps> = ({
                         height: TREEMAP_HEIGHT,
                     }}
                 >
-                    <canvas
-                        ref={chartRef}
-                        id='chart-area'
-                        width='100%'
-                        height='100%'
-                    ></canvas>
+                    <canvas id='chart-area' width='100%' height='100%'></canvas>
                 </Box>
             )}
             {!loading && chartNotLoadingError && (
