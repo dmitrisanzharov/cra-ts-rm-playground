8cde3205dba0d5b0fae5c4564a6bbc158de6d1b9
Victor Sarov <victor.sarov@rentalmatics.com>
2023-10-23 13:35:38 +0100

    all good

--- src/fakeApis/vehicleMilageByVid.js
+++ src/fakeApis/vehicleMilageByVid.js
@@ -4,13 +4,13 @@
 
 const fakeApi = [
     {
-      "registration": "172D6060",
-      "imei": 0,
-      "epm_start": 0,
-      "current_kms": 0,
-      "est_kms": 0,
-      "est_current_kms": 0
-    }
-  ];
+        registration: '172D6060',
+        imei: 0,
+        epm_start: 0,
+        current_kms: 0,
+        est_kms: 0,
+        est_current_kms: 0,
+    },
+];
 
 export default fakeApi;
