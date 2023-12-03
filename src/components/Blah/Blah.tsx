b1d3ecc557a5723df9be8fb98354c1a19f91b48f
Victor Sarov <victor.sarov@rentalmatics.com>
2023-12-03 15:30:44 +0000

    all godoerrorBoundary

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
 // @ts-ignore
 import { useCountIncrease, incCount2 } from './hooks';
-import allCarsApi from 'src/fakeApis/fleetAllVehicles';
+
 
 type Props = any;
 
 const Blah: React.FC<any> = ({ ...rest }: Props) => {
 
-    console.log(allCarsApi);
 
     return (
         <div {...rest}>
