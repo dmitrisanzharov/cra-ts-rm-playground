436ae48b540ee82587a6e9c9d1780ccfec44701b
fruitloaf <dmitrisanzharov@gmail.com>
2024-02-11 17:55:55 +0000

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,12 @@ import SideMenu from 'src/components/SideMenu/SideMenu';
 import Blah from 'src/components/Blah/Blah';
 import BasicTreemapTestMessy from 'src/components/charts/BasicTreeMapTest/BasicTreemapTestMessy';
 import BasicTreemapGoodSample from 'src/components/charts/BasicTreeMapTest/BasicTreemapGoodSample';
-import MaintenanceAlertsCard from 'src/components/cards/MaintenanceAlertsCard'
+import MaintenanceAlertsCard from 'src/components/cards/MaintenanceAlertsCard';
+
+// * REVISION
+import BasicTableRev1 from 'src/components/tableRevision/BasicTableRev1'; 
+
+
 
 function App() {
     const localStorageItemName = 'iAfg4HrIUPmMEqd_isOpen';
@@ -78,7 +83,7 @@ function App() {
                 <MaintenanceAlertsCard />
             </Box> */}
            
-            <Blah className='drr' />
+            {/* <Blah className='drr' /> */}
             {/* <BasicTreemapTestMessy /> */}
             {/* <BasicTreemapGoodSample /> */}
             {/* <hr /> */}
@@ -96,6 +101,9 @@ function App() {
             {/* <ColumnOrderingTable /> */}
             {/* <HidingColumnsTable /> */}
             {/* <FilteringColumnTableBasicInput /> */}
+
+            {/* REVISION 4 */}
+            <BasicTableRev1 />
         </Box>
     );
 }
