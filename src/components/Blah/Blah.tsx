8c76d3a7f9b89538894cf58111e0b72e771dc378
Victor Sarov <victor.sarov@rentalmatics.com>
2024-03-12 18:17:41 +0000

    clean up

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -2,27 +2,11 @@ import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
 // @ts-ignore
 
-
 type Props = any;
 
-const Blah: React.FC<any> = ({ ...rest }: Props) => {
+const Blah: React.FC<any> = (props: Props) => {
 
-    const [count2, setCount2] = React.useState(0); 
-
-    let count = 0;
-
-    function handleClick(){
-        count = count + 1; 
-        console.log('this is count', count);
-    }
-
-
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={handleClick}>inc</button>
-        </div>
-    );
+    return <div>hello</div>;
 };
 
 export default Blah;
