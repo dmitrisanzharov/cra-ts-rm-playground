28de1266296e20109627d5b3e64d7adb85ee871c
Victor Sarov <victor.sarov@rentalmatics.com>
2024-03-14 20:47:17 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -3,34 +3,33 @@ import { Box, Skeleton, Typography } from '@mui/material';
 // @ts-ignore
 // store stuff
 import { useSelector, useDispatch } from 'react-redux';
-import { countSliceActions } from 'src/store/countSlice'; 
-
+import { countSliceActions } from 'src/store/countSlice';
 
 // api stuff
 // api stuff
 import { apiSliceForUsers, useGetAllQuery } from 'src/store/api/apiSliceForUsers';
 
-
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
-
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
     const count = useSelector((state: any) => state.countSliceStateInStore.count);
 
-    const { isLoading, data, status } = useGetAllQuery();
+    // const { isLoading, data, currentData, status } = useGetAllQuery();
 
-    const oneDate = (apiSliceForUsers as any).useGetOneByIdQuery(1); // simply use hook with an argument to pass argument into query
-    console.log("oneDate: ", oneDate);
+    const data = (apiSliceForUsers as any).useLazyGetOneByIdQuery(count + 1); // simply use hook with an argument to pass argument into query
+    console.log('data', data);
+    // console.log('currentData', currentData);
+    console.log('============================');
 
-
-
-    return <div>
-        <h1>Hello This is Blah</h1>
-        <h2>The count is: {count}</h2>
-        <button onClick={()=> dispatch(countSliceActions.addToCount() as any)}>increase count</button>
-        <hr />
-    </div>;
+    return (
+        <div>
+            <h1>Hello This is Blah</h1>
+            <h2>The count is: {count}</h2>
+            <button onClick={() => dispatch(countSliceActions.addToCount() as any)}>increase count</button>
+            <hr />
+        </div>
+    );
 };
 
 export default Blah;
