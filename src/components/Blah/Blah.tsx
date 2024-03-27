81f008f84c32c01ef70a8d887306ba4814438e5e
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-27 15:19:05 +0000

    all

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
+import React from "react";
+import { Box, Skeleton, Typography } from "@mui/material";
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
+	const [reRender, setReRender] = React.useState(0);
+	const myVar1 = React.useRef<any>({ count: 0 });
 
-    return <div>hello</div>;
+
+	let myVar2 = 0;
+	
+
+	return (
+		<div>
+			<h1>Hello</h1>
+			<h2>myVar1: {myVar1.current.count}</h2>
+            <h2>ReRender: {reRender}</h2>
+			<h2>myVar2: {myVar2}</h2>
+			<button onClick={() => {
+                setReRender(reRender + 1);
+                myVar1.current.count++;
+                myVar2++;
+            }}>reRender</button>
+		</div>
+	);
 };
 
 export default Blah;
