ad0862ad0400c1a43669aeb0ba438aec01cead84
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-27 18:53:28 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+const ChildComponent: any = React.lazy(() => import('./MyChild')); 
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
-
-    return <div>hello</div>;
+	const suspenseJSX: any = <h2 style={{ color: "blue" }}>data is loading</h2>
+	return (
+		<div>
+			<h1>This is parent</h1>
+				<React.Suspense fallback={suspenseJSX}>
+					<ChildComponent />
+				</React.Suspense>
+		</div>
+	);
 };
 
 export default Blah;
+
+
+function delayForDemo(component: any) {
+	return new Promise(resolve => {
+	  setTimeout(resolve, 2000);
+	}).then(() => {
+		return component;
+	});
+  }
---
+++ src/components/Blah/MyChild.tsx
@@ -0,0 +1,10 @@
+const MyChild = (props: any) => {
+
+	return (
+		<div>
+			<h1 style={{ color: "green" }}>This is child</h1>
+		</div>
+	);
+};
+
+export default MyChild;
