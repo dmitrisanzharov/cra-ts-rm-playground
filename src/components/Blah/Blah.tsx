8df1cc985e3c8d99b332bcc60bbc8bbd3231eaa3
fruitloaf <dmitrisanzharov@gmail.com>
2024-03-28 15:48:57 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
+import React from "react";
+import { Autocomplete, TextField } from "@mui/material";
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
+	const top100Films = React.useMemo(
+		() => [
+			{ name: "The Shawshank Redemption", year: 1994 },
+			{ name: "The Godfather", year: 1972 },
+		],
+		[]
+	);
 
-    return <div>hello</div>;
+	const [value, setValue] = React.useState(top100Films[0]);
+	const [inputValue, setInputValue] = React.useState("");
+
+	React.useEffect(() => {
+		console.log("value", value);
+		console.log("inputValue", inputValue);
+	}, [value, inputValue]);
+
+	return (
+		<div>
+			<h1>This is auto complete jazz</h1>
+			<Autocomplete
+
+				// value
+				value={value}
+				onChange={(e, newValue: any) => setValue(newValue)}
+				// inputState / textFieldState
+				inputValue={inputValue}
+				onInputChange={(e, newInputValue) =>
+					setInputValue(newInputValue)
+				}
+				// all other stuff
+				sx={{ width: 300 }}
+				renderInput={(params) => {
+					return <TextField {...params} label="Movie" />;
+				}}
+
+                options={top100Films}
+				getOptionLabel={(option: any) => {
+					return option.name;
+				}}
+			/>
+		</div>
+	);
 };
 
 export default Blah;
