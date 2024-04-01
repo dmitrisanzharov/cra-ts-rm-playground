1f34d23f4fd328cac6ac924581d1ae889ba5d1b3
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-01 20:47:17 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
+import React from "react";
+import { Box, Skeleton, Typography } from "@mui/material";
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
+	const [reRender, setReRender] = React.useState(0);
+	const countRef = React.useRef<any>({ count: 0 });
+    const myDivRef = React.useRef<any>(null);
+    const myInputRef = React.useRef<any>(null);
 
-    return <div>hello</div>;
+	function handleClick() {
+		countRef.current.count++;
+		setReRender(countRef.current.count);
+        myDivRef.current.innerHTML = 'yo';
+        myInputRef.current.focus();
+	}
+
+	return (
+		<div>
+			<h1>the count is: {countRef.current.count}</h1>
+			<button onClick={handleClick}>click me</button>
+            <hr />
+            <Child ref={{ref1: myDivRef, ref2: myInputRef}} {...{name: 'blah', id: 2}}/>
+		</div>
+	);
 };
 
 export default Blah;
+
+const Child: React.FC<any> = React.forwardRef((props: any, ref: any) => {
+    // console.log('props', args);
+	return (
+		<div>
+			<h2>this is a child</h2>
+            <div ref={ref.ref1}>one</div>
+            <input ref={ref.ref2} />
+		</div>
+	);
+});
