ef06630ae70f25d52c04b1161829d567df350d36
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-01 21:26:07 +0100

    ye

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+const ChildLazy = React.lazy(() => delayForDemo(import("./Child")));
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
-
-    return <div>hello</div>;
+	return (
+		<div>
+			<h1>This is error boundary stuff</h1>
+			<hr />
+			<ErrorBoundary
+				fallback={<h1 style={{ color: "red" }}>there was an error</h1>}
+			>
+				<React.Suspense fallback={<h1 style={{color: 'green'}}>still loading</h1>}>
+					<ChildLazy />
+				</React.Suspense>
+			</ErrorBoundary>
+		</div>
+	);
 };
 
 export default Blah;
+
+function delayForDemo(component: any) {
+    return new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    }).then(() => {
+        return component;
+    });
+  }
---
+++ src/components/Blah/Child.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Child: any = (props: any) => {
+    // throw new Error('omg');
+
+    return <h1>This is child</h1>
+}
+
+export default Child;
