0005733f1008708c2bd94d62fbcb4b0328464592
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-03 19:56:13 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -5,8 +5,22 @@ import { Box, Skeleton, Typography } from '@mui/material';
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
+    const [re, setRe] = React.useState(0);
 
-    return <div>hello</div>;
+    console.log('parent');
+
+    return <div>
+        <h1>hello</h1>
+        <button onClick={()=> setRe(re+1)}>click me</button>
+        <hr />
+        <Child1 />
+    </div>;
 };
+
+
+const Child1: any = React.memo((props: any) => {
+    console.log('child1');
+    return <div>child</div>
+})
 
 export default Blah;
