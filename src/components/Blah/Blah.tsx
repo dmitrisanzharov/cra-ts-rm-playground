587fe1f7211726209776c19ff1377eeaa1a82f1e
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-11 15:40:44 +0100

    good stuff

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { ErrorBoundary } from "react-error-boundary";
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
-
-    return <div>hello</div>;
+    return <div>
+        <h1>Hello main</h1>
+        <ErrorBoundary fallback={<h2>error omfg</h2>}>
+            <Child />
+        </ErrorBoundary>
+    </div>;
 };
+
+
+const Child: any = () => {
+    throw new Error('there was error, call the cops');
+}
 
 export default Blah;
