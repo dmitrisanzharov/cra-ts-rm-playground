664b91ac8f78aa392c2d75a43c27263ecbc190bb
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-16 18:21:42 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,7 +6,20 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>hello</div>;
+    const [count, setCount] = React.useState(0);
+
+    return <div>
+        <h1>Parent, {count}</h1>
+        <button onClick={()=> setCount(count+1)}>inc</button>
+        <hr />
+        <Child />
+    </div>;
 };
+
+
+const Child: any = React.memo((props: any) => {
+    console.log('child re-rendered');
+    return <h1>Child</h1>
+});
 
 export default Blah;
