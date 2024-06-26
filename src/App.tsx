680ecb914299f4cf44a982f8deceb464fdaa5ca1
Victor Sarov <victor.sarov@rentalmatics.com>
2024-06-26 15:05:32 +0100

    all good2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
 // components
-import ReusableParent from './components/ReusableParent'
-import ChildComp from './components/ChildComp'
-
-
+import ReusableParent from './components/ReusableParent';
+import ChildComp from './components/ChildComp';
 
 function App() {
-  
     return (
-     <div>
-        <ReusableParent>
+        <div>
+            <ReusableParent>
                 <ChildComp />
-        </ReusableParent>
-     </div>
+            </ReusableParent>
+        </div>
     );
 }
 
