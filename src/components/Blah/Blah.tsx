8b94787c0c8fd995bac34984ab4162dc65b0f4ff
fruitloaf <dmitrisanzharov@gmail.com>
2024-07-24 21:02:07 +0100

    a

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
+import React from "react";
 // @ts-ignore
 
 type Props = any;
 
-const Blah: React.FC<any> = (props: Props) => {
+const useMyHook = () => {
+	const [number, setNumber] = React.useState(0);
 
-    return <div>hello</div>;
+	function handleClick() {
+		setNumber(number + 1);
+	}
+
+	return { number, handleClick };
+};
+
+const Blah: React.FC<any> = (props: Props) => {
+	const { number, handleClick } = useMyHook();
+
+	return (
+		<div>
+			<h1>{number}</h1>
+			<button onClick={handleClick}>inc</button>
+		</div>
+	);
 };
 
 export default Blah;
+
+function slowFunction(num: any) {
+	console.log("Calling slow function");
+	for (let i = 0; i <= 1000000000; i++) {}
+	return num * 2;
+}
