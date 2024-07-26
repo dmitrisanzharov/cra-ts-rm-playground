7b8a0dcc847d3a5b99cd7b1db5c12292ca777652
Victor Sarov <victor.sarov@rentalmatics.com>
2024-07-26 14:02:53 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,7 +6,23 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>hello</div>;
+    
+    // const [count, setCount] = React.useState(0);
+
+
+
+    const [myObj, setmyObj] = React.useState({start: 1, end: 10});
+
+    React.useEffect(() => {
+        console.log('triggered');
+    }, [myObj]);
+
+    return <div>
+        {/* <h1>Count: {count}</h1>
+        <hr /> */}
+        <h1>Start {JSON.stringify(myObj)}</h1>
+        <button onClick={()=> setmyObj({...myObj, start: myObj.start + 1})}>change</button>
+    </div>;
 };
 
 export default Blah;
