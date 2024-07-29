64059cf3bb998225c8a85745c3403ec570f28ccc
fruitloaf <dmitrisanzharov@gmail.com>
2024-07-29 21:19:19 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,9 +6,33 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const childRef = React.useRef<any>({});
+    const childRef2 = React.useRef<any>({});
+
+
+
+    React.useEffect(() => {
+        console.log('childRef', childRef.current);
+        if(childRef.current.style){
+            childRef.current.style.color = 'red'
+        }
+
+        if(childRef2.current.style){
+            childRef2.current.style.color = 'blue'
+        }
+    }, []);
+    
     return <div>
-        <h1>Hello</h1>
+        <Child ref={{ref1: childRef, ref2: childRef2} as any} inputProps={{placeholder: 'hello'}} />
     </div>;
 };
 
 export default Blah;
+
+
+const Child = React.forwardRef((props: any, ref: any) => {
+    return <div>
+        <h1 ref={ref.ref2}>Hello World</h1>
+        <input ref={ref.ref1} {...props.inputProps} />
+    </div>;
+})
