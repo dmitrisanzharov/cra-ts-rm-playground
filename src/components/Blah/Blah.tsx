fda6a3b5ae7e885a0027c1b6d87b5f17b5f74b04
fruitloaf <dmitrisanzharov@gmail.com>
2024-07-31 20:13:04 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
+import React from "react";
+import { Box, Skeleton, Typography } from "@mui/material";
+import { ErrorBoundary } from "react-error-boundary";
+const Child1Lazy = React.lazy(() => import("./Child1"));
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
+	const errorJSX: any = <h1>error</h1>;
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+	return (
+		<div>
+			<h1>Hello</h1>
+			<ErrorBoundary fallback={errorJSX}>
+				<React.Suspense fallback={<h1 style={{ color: "red" }}>Loading</h1>}>
+					<Child1Lazy />
+				</React.Suspense>
+			</ErrorBoundary>
+		</div>
+	);
 };
 
 export default Blah;
+
+function delayForDemo(component: any) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, 2000);
+	}).then(() => {
+		return component;
+	});
+}
---
+++ src/components/Blah/Child1.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Child1 = () => {
+	// throw new Error("omg");
+
+	return <h1>Child1</h1>;
+};
+
+export default Child1;
