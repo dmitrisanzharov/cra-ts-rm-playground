cedcfc9a1444c70f38cce7da092afc8cb7a1fac2
fruitloaf <dmitrisanzharov@gmail.com>
2024-08-02 23:17:14 +0100

    a

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -12,46 +12,37 @@ const Blah: React.FC<any> = (props: Props) => {
 		<div>
 			<h1>....</h1>
 			<Autocomplete
-
 				renderInput={(params) => {
 					return <TextField {...params} label="numbers" />;
 				}}
 				sx={{ width: "50vw" }}
-
 				//   YOUTUBE DEMO
 				options={["1", "2", "3"]} // joe, jane, foo
+				filterOptions={(originalOption: any, state: any) => {
+					if (state.inputValue === "names") {
+						return ["foo", "joe", "jane"];
+					}
 
-                filterOptions={(originalOption: any, state: any) => {
+					return originalOption;
+				}}
+				isOptionEqualToValue={(originalOptionsArrayValue: any, valueSelectedByUser: any) => {
+					console.log("originalOptionsArray", originalOptionsArrayValue);
+					console.log("valueSelectedByUser", valueSelectedByUser);
 
-                    if(state.inputValue === 'names'){
-                        return ['foo', 'joe', 'jane'];
-                    }
+					if (originalOptionsArrayValue === "1" && valueSelectedByUser === "joe") {
+						return true;
+					}
 
-                    return originalOption;
-                }}
+					if (valueSelectedByUser === "jane" && originalOptionsArrayValue === "2") {
+						return true;
+					}
 
-                isOptionEqualToValue={(originalOptionsArrayValue: any, valueSelectedByUser: any) => {
+					if (valueSelectedByUser === "foo" && originalOptionsArrayValue === "3") {
+						return true;
+					}
 
-                    console.log('originalOptionsArray', originalOptionsArrayValue);
-                    console.log('valueSelectedByUser', valueSelectedByUser);
-
-                    if(originalOptionsArrayValue === '1' && valueSelectedByUser === 'joe'){
-                        return true;
-                    }
-                    
-                    if(valueSelectedByUser === 'jane' && originalOptionsArrayValue === "2"){
-                        return true;
-                    }
-
-                    if(valueSelectedByUser === 'foo' && originalOptionsArrayValue === "3"){
-                        return true;
-                    }
-
-                    return false;
-
-                }}
-
-
+					return false;
+				}}
 			/>
 		</div>
 	);
