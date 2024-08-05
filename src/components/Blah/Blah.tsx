c51f139f727a3dc396dbc853d315537cf33b4437
fruitloaf <dmitrisanzharov@gmail.com>
2024-08-05 16:47:06 +0100

    ll

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -4,11 +4,37 @@ import { Box, Skeleton, Typography } from '@mui/material';
 
 type Props = any;
 
+const useMyHook = (arg: any) => {
+    const [count, setCount] = React.useState(0);
+
+    function myFunction(){
+        setCount(count+1)
+    }
+
+    React.useMemo(()=> console.log('myArg', arg), [arg]);
+
+    return {count, myFunction}
+}
+
 const Blah: React.FC<any> = (props: Props) => {
 
+
+    const {count, myFunction} = useMyHook(2);
+
+    const troll = useMyHook(3)
+
     return <div>
-        <h1>Hello</h1>
+        <h1>Hello {troll.count}</h1>
+        <h1>{count}</h1>
+        <button onClick={myFunction}>inc</button>
     </div>;
 };
 
 export default Blah;
+
+
+function slowFunction(num: any){
+    console.log('Calling slow function');
+    for(let i = 0; i <= 1000000000; i++){}
+    return num * 2;
+   }
