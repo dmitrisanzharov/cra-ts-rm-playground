3ed61e61e4dc45fe03a94c73676835956c220e98
Victor Sarov <victor.sarov@rentalmatics.com>
2024-09-22 14:26:40 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,8 +6,20 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const [count, setCount] = React.useState(0)
+
+    const [myObj, setMyObj] = React.useState({name: 1}); // x0xx
+
+    const myObj2 = {name: 1}; // a0aa -> a1aa -> a2aa
+
+    React.useEffect(() => {
+        console.log('ran');
+        setCount(count + 1)
+    }, [JSON.stringify(myObj)]);
+
     return <div>
-        <h1>Hello</h1>
+        <h1>Hello, {myObj.name} ... count: {count}</h1>
+        <button onClick={()=> setMyObj({name: Math.random()})}>change</button>
     </div>;
 };
 
