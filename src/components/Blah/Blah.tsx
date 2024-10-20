907adac4e9cfe921ff9718a32df51e20e01819f6
fruitloaf <dmitrisanzharov@gmail.com>
2024-10-20 16:48:54 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import {useHookOne} from './hooks';
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const [count, setCount] = React.useState(0);
+
+    const omg = useHookOne();
+
     return <div>
         <h1>Hello</h1>
+        <h1>{count}</h1>
+        <button onClick={()=> setCount(count+1)}>inc</button>
     </div>;
 };
 
--- src/components/Blah/hooks.ts
+++ src/components/Blah/hooks.ts
@@ -7,3 +7,10 @@ export const useCountIncrease = (state: any, setState: any) => {
 export const incCount2 = (state: any, setState: any): any => {
     setState(state + 1);
 };
+
+
+export const useHookOne = () => {
+    React.useMemo(()=> console.log('omg'), [])
+
+    console.log('omg2')
+}
