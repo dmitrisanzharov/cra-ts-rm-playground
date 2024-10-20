e9b1c423f7334143d77ef86633f705c502519460
fruitloaf <dmitrisanzharov@gmail.com>
2024-10-20 16:04:25 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import Foo from './Foo';
 // @ts-ignore
+
+// memo component
+const FooMemo = React.memo(()=> <Foo />);
+
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const [count, setCount] = React.useState(0);
+
+  
+
+
     return <div>
         <h1>Hello</h1>
+        <h1>{count}</h1>
+        <button onClick={()=> setCount(count+1)}>inc</button>
+        <hr />
+        <FooMemo />
+
     </div>;
 };
 
---
+++ src/components/Blah/Foo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Foo = (props: Props) => {
+
+    console.log('foo')
+
+  return (
+    <div>Foo</div>
+  )
+}
+
+export default Foo
