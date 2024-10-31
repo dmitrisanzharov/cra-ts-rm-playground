2d6dd66f027255e74ff9a00195ec6feb9556ae1f
Victor Sarov <victor.sarov@rentalmatics.com>
2024-10-31 13:19:32 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { useCustomHook, useMyHook2 } from './hooks';
 // @ts-ignore
 
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+    const myHook = useMyHook2('hello');
+
+    return (
+        <div>
+            <h1>Parent component, count is: {myHook.number}</h1>
+            <button onClick={myHook.handleClick}>inc</button>
+            <hr />
+        </div>
+    );
 };
 
 export default Blah;
+
+function slowFunction(num: any) {
+    console.log('Calling slow function');
+    for (let i = 0; i <= 1000000000; i++) {}
+    return num * 2;
+}
--- src/components/Blah/hooks.ts
+++ src/components/Blah/hooks.ts
@@ -7,3 +7,23 @@ export const useCountIncrease = (state: any, setState: any) => {
 export const incCount2 = (state: any, setState: any): any => {
     setState(state + 1);
 };
+
+
+export const useCustomHook = (fn: any, arg: number) => {
+    return React.useMemo(()=> {
+        console.log('fn called')
+        return fn(2)
+    }, [])
+}
+
+export const useMyHook2 = (arg: string) => {
+    const [number, setNumber] = React.useState(0);
+
+    function handleClick() {
+        setNumber(number + 1);
+    }
+
+    console.log('arg', arg);
+
+    return {number, handleClick}
+}
