c0df9dc6026ce7eed39582258c95e263bb0ab55d
Victor Sarov <victor.sarov@rentalmatics.com>
2024-12-10 19:35:20 +0000

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,73 @@
-import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { styled, SxProps } from '@mui/material';
 
-type Props = any;
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
 
-const Blah: React.FC<any> = (props: Props) => {
+const tabsSx: SxProps = {
+    '& .MuiButtonBase-root.MuiTab-root.Mui-selected': { // same as: textColor
+        color: 'orange', 
+    },
+    '& .MuiTabs-indicator': { // same as: indicatorColor
+        backgroundColor: 'green',
+    },
+ };
+ 
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
-};
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-export default Blah;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ p: 4 }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} textColor={'primary'} indicatorColor={'secondary'} sx={tabsSx}>
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </Box>
+  );
+}
