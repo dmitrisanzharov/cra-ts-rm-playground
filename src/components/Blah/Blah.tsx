f54ef5f9d254abc3efee6d95c6c4c37aec0615f0
fruitloaf <dmitrisanzharov@gmail.com>
2025-01-28 22:15:30 +0000

    Update Blah.tsx

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { boxSizing } from '@mui/system';
 // @ts-ignore
 
 type Props = any;
 
+// SX
+const containerSx: any = {
+    // display: 'flex',
+    // border: '10px solid black',
+    // height: '90vh',
+    // alignItems: 'center'
+}
+
+const boxSx: any = {
+    width: '150px',
+    height: '150px',
+    fontSize: '8rem',
+    borderRadius: '15px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+    return <Box sx={containerSx}>
+        <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
+        <Box sx={{...boxSx, backgroundColor: 'yellow'}}>2</Box>
+        <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
+        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
+    </Box>;
 };
 
 export default Blah;
+
+
+
