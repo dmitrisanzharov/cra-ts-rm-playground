a540092bee0c571a338b731670411594844e1547
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-08 14:03:56 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
+import { flexbox } from '@mui/system';
 // @ts-ignore
+
 
 type Props = any;
 
+
+// SX
+const containerSx: any = {
+    border: '10px solid black',
+    padding: '5px',
+    margin: '10px',
+
+    display: 'flex',
+    // justifyContent: 'right',
+
+    height: '90vh',
+    // alignItems: 'center',
+
+}
+
+
+const boxSx: any = {
+    // width: '150px',
+    // height: '150px',
+    fontSize: '8rem',
+    borderRadius: '15px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+}
+
+
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+
+    return <Box sx={containerSx}>
+        <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
+        <Box sx={{...boxSx, backgroundColor: 'yellow', alignSelf: 'start'}}>2</Box>
+        <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
+        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
+    </Box>;
 };
+
 
 export default Blah;
