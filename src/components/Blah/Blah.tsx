d830e2b3f6f09ca1849e05aa2e5f07c3ae5d6df0
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-11 13:05:41 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -17,14 +17,14 @@ const containerSx: any = {
     // flexWrap: 'wrap',
     // justifyContent: "space-evenly",
 
-    height: "90vh",
+    height: "90vh"
     // alignItems: 'start',
     // alignContent: 'start'
 };
 
 const boxSx: any = {
-    width: '150px',
-    height: '150px',
+    width: "150px",
+    height: "150px",
     // fontSize: "8rem",
     borderRadius: "15px",
     display: "flex",
@@ -33,23 +33,58 @@ const boxSx: any = {
     flex: 1
 };
 
+// const Blah: React.FC<any> = (props: Props) => {
+//     return (
+//         <Box sx={containerSx}>
+//             <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
+//             {/* <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
+//             {/*  */}
+//             {/* <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
+//             {/*  */}
+//             {/* <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
+//             <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
+//         </Box>
+//     );
+// };
+
 const Blah: React.FC<any> = (props: Props) => {
+    const containerSx = {
+        display: "flex",
+        flexWrap: "wrap",
+        gap: '5px',
+        margin: '5px',
+        "& *": {
+            border: "1px solid black",
+            minHeight: "20vh"
+        }
+    };
+
+    const bigBox = {
+        flex: 2,
+        minWidth: "98vw"
+    };
+
+    const smallBox = {
+        flex: 1,
+        minWidth: '48vw'
+    };
+
     return (
         <Box sx={containerSx}>
-            <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
-            {/* <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
-            {/*  */}
-            {/* <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
-            {/*  */}
-            {/* <Box sx={{ ...boxSx, backgroundColor: "red" }}>1</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "yellow" }}>2</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "green" }}>3</Box>
-            <Box sx={{ ...boxSx, backgroundColor: "blue" }}>4</Box> */}
+            <Box sx={bigBox}>1</Box>
+            <Box sx={smallBox}>2</Box>
+            <Box sx={smallBox}>3</Box>
+
+            <Box sx={smallBox}>4</Box>
+            <Box sx={smallBox}>5</Box>
+            <Box sx={bigBox}>6</Box>
         </Box>
     );
 };
