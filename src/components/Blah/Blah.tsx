c707d5b4e90c4a848a7df7069732a6febca6d614
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-30 09:34:24 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
 
 type Props = any;
 
+let a = 1;
+
+
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+
+   console.log('parent mounted');
+
+
+   React.useEffect(() => {
+       console.log('parent useEffect ran');
+   }, []);
+
+
+   return <div>
+       <h1>Hello</h1>
+       <Child1 />
+   </div>;
 };
 
+
 export default Blah;
+
+
+
+
+const Child1 = (props: any) => {
+
+
+   console.log('child mounted');
+
+
+   React.useEffect(() => {
+       console.log('child useEffect ran');
+   }, []);
+
+
+   return <div>
+       <h1>Child1</h1>
+   </div>;
+}
