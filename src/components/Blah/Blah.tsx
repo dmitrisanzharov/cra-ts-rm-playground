3e62815b626f289738323af297144bc973a75a66
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-31 18:01:55 +0100

    good stuff

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -9,6 +9,8 @@ const Blah: React.FC<any> = (props: Props) => {
     const htmlElement = React.useRef<HTMLDivElement>(null);
     console.log("htmlElement: ", htmlElement);
 
+    
+
 
     return <div>
         <h1 ref={htmlElement}>Hello</h1>
