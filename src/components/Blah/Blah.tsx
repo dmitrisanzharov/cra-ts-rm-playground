52910cadf2693c6da5d65a7899e25e1867c6c8f0
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-01 09:22:36 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -5,16 +5,32 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    // const [count, setCount] = React.useState(0);
+
+    const count = React.useRef(0)
+
 
     const htmlElement = React.useRef<HTMLDivElement>(null);
     console.log("htmlElement: ", htmlElement);
 
+    // htmlElement.current.style.color = "red";
 
+
+    React.useEffect(() => {
+        if(htmlElement.current){
+            htmlElement.current.style.color = "red";
+        }
+    }, []);
+
+    React.useEffect(() => {
+        console.log("Count changed: ", count);
+    }, [count]);
 
 
     return <div>
         <h1 ref={htmlElement}>Hello</h1>
+        <button onClick={() => count.current = count.current + 1}>Increment</button>
     </div>;
-};
+};  
 
 export default Blah;
