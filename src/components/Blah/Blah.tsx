795625cff8086f0b37e6c31e7d6d7b74be73df6b
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-09 15:39:18 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+
 
 type Props = any;
 
+
+let a = 1;
+
+
+
+
 const Blah: React.FC<any> = (props: Props) => {
 
-    const [count, setCount] = React.useState(0);
+    a = 2;
 
-    setCount(0);
+    a = 3;
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+
+   return <div>
+       <h1>Hello</h1>
+       <button onClick={() => {
+        console.log('a', a)
+        a = a + 1
+       }}>change a</button>
+       <Child1 a={a}  />
+   </div>;
 };
 
+
 export default Blah;
+
+
+function Child1(props: any) {
+
+
+    React.useEffect(() => {
+        console.log('child1 useEffect re-rendered', props.a);
+    }, [props.a]);
+
+
+   console.log('child re-rendered');
+   return <div>child1</div>
+}
