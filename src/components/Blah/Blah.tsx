9e608c7f0f613e6f1e844b3a48ec994665d94f94
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-09 16:55:32 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -11,19 +11,16 @@ let a = 1;
 
 const Blah: React.FC<any> = (props: Props) => {
 
-    a = 2;
+    console.log('Comp1');
 
-    a = 3;
+    React.useEffect(() => {
+        console.log('in useEffect Comp1');
+    }, []);
 
-
-   return <div>
-       <h1>Hello</h1>
-       <button onClick={() => {
-        console.log('a', a)
-        a = a + 1
-       }}>change a</button>
-       <Child1 a={a}  />
-   </div>;
+    return <div>
+        <h1>Hello</h1>
+        <Child1 />
+    </div>;
 };
 
 
@@ -33,11 +30,10 @@ export default Blah;
 function Child1(props: any) {
 
 
+    console.log('Comp2');
+
     React.useEffect(() => {
-        console.log('child1 useEffect re-rendered', props.a);
-    }, [props.a]);
-
-
-   console.log('child re-rendered');
-   return <div>child1</div>
+        console.log('in useEffect Comp2');
+    }, []);
+    return <div>child1</div>
 }
