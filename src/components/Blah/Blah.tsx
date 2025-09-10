767e48226af8a21931fb2079b4a9e58c0ff4307f
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-10 14:58:16 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,9 +6,19 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const [count, setCount] = React.useState(0);
+
+    const myObj1 = React.useMemo(()=> ({}), []);
+
+    React.useEffect(() => {
+        console.log('re-rendering');
+        setCount(count + 1);
+    }, [myObj1]);
+
     return <div>
         <h1>Hello</h1>
     </div>;
 };
 
 export default Blah;
+
