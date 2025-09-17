a26bb3b22aa854b52a6836c6460ab6707bbdf838
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-17 11:21:52 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+
 
 type Props = any;
 
+
 const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+  const [count, setCount] = React.useState(0);
+  
+
+  React.useEffect(() => {
+    setCount(prev => prev + 2);
+    setCount(prev => prev + 5);
+    setCount(prev => prev + 3);
+  }, []);
+
+  React.useEffect(() => {
+    console.log('count has changed', count);
+  }, [count]);
+
+   return <div>
+       <h1>Hello</h1>
+   </div>;
 };
 
 export default Blah;
+
+
+
+
+// function Child1(props: any) {
+
+
+//    console.log('child re-rendered');
+//    return <div>child1</div>
+// }
+
