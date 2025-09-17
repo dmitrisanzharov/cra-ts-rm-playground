8a1c9c18eeb3e82e7fa713a9643ea66647c7a63c
VictorSarov <victor.sarov@rentalmatics.com>
2025-09-17 11:50:33 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,21 +6,23 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
-  const [count, setCount] = React.useState(0);
+//   const [count, setCount] = React.useState(0);
   
 
-  React.useEffect(() => {
-    setCount(prev => prev + 2);
-    setCount(prev => prev + 5);
-    setCount(prev => prev + 3);
-  }, []);
+//   React.useEffect(() => {
+//     setCount(prev => prev + 2);
+//     setCount(prev => prev + 5);
+//     setCount(prev => prev + 3);
+//   }, []);
 
   React.useEffect(() => {
-    console.log('count has changed', count);
-  }, [count]);
+    console.log('SECOND');
+  }, []);
 
    return <div>
        <h1>Hello</h1>
+       <Child1 />
+       <Child1 />
    </div>;
 };
 
@@ -29,10 +31,12 @@ export default Blah;
 
 
 
-// function Child1(props: any) {
+function Child1(props: any) {
 
-
-//    console.log('child re-rendered');
-//    return <div>child1</div>
-// }
+React.useEffect(() => {
+    console.log('FIRST');
+}, []);
+   
+   return <div>child1</div>
+}
 
