77e8b6349367b72276775c13fc8a01aabb67fe5f
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-16 09:20:44 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,6 +6,12 @@ type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    console.log('re-render');
+
+    const [count, setCount] = React.useState(0);
+    // setCount(0);
+    
+
     return <div>
         <h1>Hello</h1>
     </div>;
