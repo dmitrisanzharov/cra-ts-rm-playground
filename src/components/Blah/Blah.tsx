dfa1c58b135c29ceaa3c11c4ce74f2a7c6f88c8e
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-17 10:55:09 +0100

    all done

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -4,17 +4,50 @@ import { Box, Skeleton, Typography } from '@mui/material';
 
 type Props = any;
 
+let a = 1;
+
 const Blah: React.FC<any> = (props: Props) => {
+
+    a = 2;
+    a = 3;
 
     console.log('re-render');
 
     const [count, setCount] = React.useState(0);
     // setCount(0);
-    
+
+React.useEffect(() => {
+    console.log('useEffect parent');
+    setCount(prev => prev + 2);
+    setCount(prev => prev + 5);
+    setCount(prev => prev + 3);
+  }, []);
+
+    // React.useEffect(() => {
+    //     setCount(0);
+    //     console.log('mah man')
+    // }, []);
+
 
     return <div>
-        <h1>Hello</h1>
+        <h1>Hello {count}</h1>
+        {/* <button onClick={}>Increment</button> */}
+        <Child a={a} />
     </div>;
 };
 
 export default Blah;
+
+
+const Child: any = ({ a }: any) => {
+    console.log('a render');
+
+    React.useEffect(() => {
+    console.log('useEffect child');
+  }, []);
+
+      React.useEffect(() => {
+    console.log('useEffect child');
+  }, []);
+    return <div>Child {a}</div>;
+}
