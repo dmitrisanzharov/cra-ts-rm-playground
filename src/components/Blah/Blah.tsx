7b95691dd0269a65315ce9a5eeb4b3db2ec420f3
VictorSarov <victor.sarov@rentalmatics.com>
2025-10-28 12:25:20 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -12,9 +12,9 @@ const containerSx: any = {
     padding: '20px',
     display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'start',
-    alignContent: 'start',
+    // flexWrap: 'wrap',
+    // alignItems: 'start',
+    // alignContent: 'start',
     // justifyContent: 'right',
     // border: '10px solid black',
     // height: '90vh',
@@ -26,7 +26,7 @@ const containerSx: any = {
 const boxSx: any = {
     width: '150px',
     height: '150px',
-    fontSize: '8rem',
+    // fontSize: '8rem',
     borderRadius: '15px',
     display: 'flex',
     justifyContent: 'center',
@@ -37,8 +37,8 @@ const boxSx: any = {
 const Blah: React.FC<any> = (props: Props) => {
 
     return <Box sx={containerSx}>
-        <Box sx={{ ...boxSx, backgroundColor: 'red' }}>1</Box>
-        <Box sx={{ ...boxSx, backgroundColor: 'yellow' }}>2</Box>
+        <Box sx={{ ...boxSx, backgroundColor: 'red', flexGrow: 2, flexShrink: 4 }}>1</Box>
+        <Box sx={{ ...boxSx, backgroundColor: 'yellow', flexShrink: 1}}>2</Box>
         <Box sx={{ ...boxSx, backgroundColor: 'green' }}>3</Box>
         <Box sx={{ ...boxSx, backgroundColor: 'blue' }}>4</Box>
     </Box>;
