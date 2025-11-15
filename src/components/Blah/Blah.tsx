6f9720470da57e6626f99eeb19c704891090dc4b
VictorSarov <victor.sarov@rentalmatics.com>
2025-11-15 08:16:56 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -4,11 +4,13 @@ import { Box, Skeleton, Typography } from '@mui/material';
 
 type Props = any;
 
-const Blah: React.FC<any> = (props: Props) => {
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+const Blah: React.FC<any> = (props: Props) => {
+    return (
+        <div>
+            <h3>Hello</h3>
+        </div>
+    );
 };
 
 export default Blah;
