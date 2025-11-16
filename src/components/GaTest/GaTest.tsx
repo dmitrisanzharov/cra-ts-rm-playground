12286d2c5048bb02fe58831ee53d0bb3999e11e6
VictorSarov <victor.sarov@rentalmatics.com>
2025-11-16 08:04:24 +0000

    good stuff

--- src/components/GaTest/GaTest.tsx
+++ src/components/GaTest/GaTest.tsx
@@ -8,10 +8,12 @@ import Typography from "@mui/material/Typography";
 
 type Props = any;
 
-ReactGA.initialize("G-XXXXXXXXXX");
+
+ReactGA.initialize("G-3B58WKHYG4");
 
 const GaTest: React.FC<any> = (props: Props) => {
     const handleClick = () => {
+        console.log("clicked");
         ReactGA.event({
             category: "Button",
             action: "Click",
@@ -19,10 +21,20 @@ const GaTest: React.FC<any> = (props: Props) => {
         });
     };
 
+    const handleClick2 = () => {
+        console.log("clicked 2");
+        ReactGA.event({
+            category: "Button 2",
+            action: "Click2",
+            label: "Special Button 2"
+        });
+    };
+
     return (
         <div>
             <h1>Hello</h1>
             <button onClick={handleClick}>Click me</button>
+            <button onClick={handleClick2}>Click me 2</button>
         </div>
     );
 };
