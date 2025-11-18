ba5a77e5f2b66fd75cd317c3218d2bd1ad87add4
VictorSarov <victor.sarov@rentalmatics.com>
2025-11-18 10:14:20 +0000

    fixed

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,33 +1,43 @@
 import React, { useEffect } from 'react';
-import { Box, Skeleton, Typography } from '@mui/material';
-import OpenAI from "openai";
+import { Box, Skeleton, Typography, Button } from '@mui/material';
+import Groq from 'groq-sdk';
 // @ts-ignore
 
 type Props = any;
 
-const client = new OpenAI({ apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
-
-
 const Blah: React.FC<any> = (props: Props) => {
+    
+    const client2 = new Groq({
+        apiKey: process.env.REACT_APP_GROQ_API_KEY,
+        dangerouslyAllowBrowser: true
+    });
+    
+    console.log('client2: ', process.env.REACT_APP_GROQ_API_KEY);
 
     async function handler(message: string) {
-        const reply = await client.chat.completions.create({
-            model: "gpt-4.1-mini",
-            messages: [{ role: "user", content: message }]
+        const reply = await client2.chat.completions.create({
+            model: 'llama-3.1-8b-instant',
+            messages: [
+                {
+                    role: 'system',
+                    content:
+                        'You are an expert that only knows the following mapping:\n- red → apple\n- yellow → banana\n- green → kale\n\nWhen the user gives you a color, reply ONLY with the corresponding item. \nIf the color is not in the mapping, reply "I don\'t know".'
+                },
+                { role: 'user', content: message }
+            ]
         });
-
-        console.log('reply: ', reply);
+        console.log('🚀 ~ handler ~ reply:', reply);
+        console.log('reply: ', reply?.choices?.[0]?.message?.content);
 
         return reply;
     }
 
-    useEffect(() => {
-        handler('Hello, how are you?');
-    }, []);
-
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+    return (
+        <div>
+            <h1>Hello</h1>
+            <Button onClick={() => handler('yellow')}>request</Button>
+        </div>
+    );
 };
 
 export default Blah;
