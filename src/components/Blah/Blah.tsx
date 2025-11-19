8adbe8f6a336089a780ef456a25888359cc9d85a
VictorSarov <victor.sarov@rentalmatics.com>
2025-11-19 08:55:31 +0000

    good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,27 @@
 import React, { useEffect } from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-2W2DYGZ5V6");
+type GAEventParams = Parameters<typeof ReactGA.event>[1];
 
 type Props = any;
 
 
 const Blah: React.FC<any> = (props: Props) => {
 
+    const handleClick = () => {
+        console.log("Button clicked");
+        ReactGA.event({
+            category: "User",
+            action: "click on 19-Nov-2025",
+            label: "Blah Component Button"
+        } as GAEventParams);
+    }
+
     return <div>
         <h1>Hello</h1>
+        <button onClick={handleClick} >Click Me</button>
     </div>;
 };
 
