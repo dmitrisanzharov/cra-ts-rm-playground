94a0a60fbcd90157726982e1cbdb5c07b141023b
VictorSarov <victor.sarov@rentalmatics.com>
2025-12-06 15:09:53 +0000

    all good

---
+++ src/components/Blah/contextString.ts
@@ -0,0 +1,9 @@
+export const contextString = `
+You are an expert that only knows the following mapping:
+- red → apple
+- yellow → banana
+- green → kale
+
+When the user gives you a color, reply ONLY with the corresponding item.
+If the color is not in the mapping, reply "I don't know".
+`;
