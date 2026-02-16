f3bbc74f2fd8cc8338936da4ad4bb61f88f386f2
fruitloaf <dmitrisanzharov@gmail.com>
2026-02-16 09:23:42 +0000

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,37 @@ type Props = any;
 const containerSx: any = {
     display: 'flex',
     flexDirection: 'row',
+    padding: '20px',
     // justifyContent: 'right',
     border: '10px solid black',
+        gap: '30px',
+        flexWrap: 'wrap',
     // flexWrap: 'wrap',
     // alignContent: 'flex-end',
     // alignItems: 'flex-start',
     // margin: '10px',
-    // marginLeft: '25vw'
+    // marginLeft: '25vw',
+    // '@media (max-width: 700px)': {
+    //     flexDirection: 'column',
+    //     // alignItems: 'center',
+    //     // justifyContent: 'center',
+    // },
 
 
 }
 
 
 const boxSx: any = {
-    // width: '150px',
-    // height: '150px',
-    fontSize: '8rem',
-    lineHeight: '8rem',
+    width: '450px',
+    height: '150px',
+    fontSize: '10rem',
+    lineHeight: '10rem',
     borderRadius: '15px',
     // display: 'flex',
     // justifyContent: 'center',
     // alignItems: 'center',
     // flex: 1,
-    flexBasis: 1,
+
 }
 
 
@@ -42,8 +50,8 @@ const Blah: React.FC<any> = (props: Props) => {
     return <Box sx={containerSx}>
         <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
         <Box sx={{...boxSx, backgroundColor: 'yellow'}}>2</Box>
-        <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
-        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
+        {/* <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
+        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box> */}
     </Box>;
 };
 
