b58b581baf3eca553823944547e0a8b228ee81cc
fruitloaf <dmitrisanzharov@gmail.com>
2026-02-16 08:11:18 +0000

    all good

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ type Props = any;
 // SX
 const containerSx: any = {
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     // justifyContent: 'right',
     border: '10px solid black',
     height: '95vh',
-    flexWrap: 'wrap-reverse'
-    // alignItems: 'center',
+    // flexWrap: 'wrap',
+    // alignContent: 'flex-end',
+    // alignItems: 'flex-start',
     // margin: '10px',
     // marginLeft: '25vw'
 
@@ -26,11 +27,12 @@ const boxSx: any = {
     width: '150px',
     height: '150px',
     fontSize: '8rem',
+    lineHeight: '8rem',
     borderRadius: '15px',
     // display: 'flex',
     // justifyContent: 'center',
     // alignItems: 'center',
-    // flex: 1
+    flex: 1,
 }
 
 
@@ -39,7 +41,7 @@ const Blah: React.FC<any> = (props: Props) => {
 
     return <Box sx={containerSx}>
         <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
-        <Box sx={{...boxSx, backgroundColor: 'yellow', alignSelf: 'flex-start'}}>2</Box>
+        <Box sx={{...boxSx, backgroundColor: 'yellow'}}>2</Box>
         <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
         <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
     </Box>;
