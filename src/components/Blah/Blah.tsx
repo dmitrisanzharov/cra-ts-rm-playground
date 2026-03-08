fb3947c65a8637bd599af8d56e589e5e8d1d49da
fruitloaf <dmitrisanzharov@gmail.com>
2026-03-08 12:46:52 +0000

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
 // @ts-ignore
+// tables
+import BasicTable from 'src/components/table/BasicTable';
 
 type Props = any;
+
+
 
 
 const Blah: React.FC<any> = (props: Props) => {
 
     return <div>
-        <h1>Hello</h1>
+       <BasicTable />
     </div>;
 };
 
