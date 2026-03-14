fc6d6b2482dc652eafb0891096e606e459c471f3
VictorSarov <victor.sarov@rentalmatics.com>
2026-03-14 12:37:19 +0000

    g

--- src/components/Blah/BlahTable.tsx
+++ src/components/Blah/BlahTable.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-    Table as MuiTable,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-} from '@mui/material';
+import { Table as MuiTable, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import {
     useReactTable,
     flexRender,
@@ -16,40 +8,49 @@ import {
     getCoreRowModel,
     getFilteredRowModel
 } from '@tanstack/react-table';
-import { columnDef } from './blahColumns';
+import { columnDef, columnDefWithGroup } from './blahColumns';
 import dataJson from '../table/data';
 
 type Props = {};
 
+const initialColumnVisibility = {
+    first_name: false
+};
+
 const BasicTable = (props: Props) => {
     const data: any = React.useMemo(() => dataJson, []);
     const columns: any = React.useMemo(() => columnDef, []);
+    const [columnVisibility, setColumnVisibility] = React.useState<any>(initialColumnVisibility);
 
     const table: Table<any> = useReactTable({
         data,
         columns,
+        state: {
+            columnVisibility: columnVisibility
+        },
         getCoreRowModel: getCoreRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
+        onColumnVisibilityChange: setColumnVisibility
     } as TableOptions<any>);
 
-
     // consoles
-    console.log('test columns', table.options.columns);
-    console.log('test a', table.getAllColumns());
-    console.log('test2', table.options.data);
-    console.log('filters', table.getState().columnFilters);
+    // console.log('test columns', table.options.columns);
+    // console.log('test a', table.getAllColumns());
+    // console.log('test2', table.options.data);
+    // console.log('filters', table.getState().columnFilters);
 
     React.useEffect(() => {
         table.setColumnFilters([
             {
-            id: 'first_name',
-            value: 'Arlene'
-        }
-        ])
+                id: 'first_name',
+                value: 'Arlene'
+            }
+        ]);
     }, []);
 
     return (
         <TableContainer component={Paper}>
+            <button onClick={() => setColumnVisibility({ ...columnVisibility, first_name: !columnVisibility.first_name })}>toggle first_name</button>
             <MuiTable>
                 <TableHead sx={{ border: '3px solid red' }}>
                     {table.getHeaderGroups().map((headerGroupEl) => {
@@ -65,11 +66,7 @@ const BasicTable = (props: Props) => {
                                             // }}
                                             colSpan={columnEl.colSpan}
                                         >
-                                            {flexRender(
-                                                columnEl.column.columnDef
-                                                    .header,
-                                                columnEl.getContext()
-                                            )}
+                                            {flexRender(columnEl.column.columnDef.header, columnEl.getContext())}
                                         </TableCell>
                                     );
                                 })}
@@ -84,10 +81,7 @@ const BasicTable = (props: Props) => {
                                 {row.getVisibleCells().map((cell) => {
                                     return (
                                         <TableCell key={cell.id}>
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
-                                            )}
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     );
                                 })}
--- src/components/Blah/blahColumns.tsx
+++ src/components/Blah/blahColumns.tsx
@@ -1,6 +1,23 @@
 import { createColumnHelper } from '@tanstack/react-table';
 
-const columnHelper = createColumnHelper<any>();
+type personItem = {
+    first_name: string;
+    last_name: string;
+    email: string;
+    gender: string;
+};
+
+const columnHelper = createColumnHelper<personItem>();
+// console.log("columnHelper: ", columnHelper);
+
+function myFn(row: any, columnId: any, filterValue: any) {
+    console.log('============================');
+    console.log('row: ', row);
+    console.log('columnId: ', columnId);
+    console.log('filterValue: ', filterValue);
+
+    return true;
+}
 
 // * BASIC TABLE
 
@@ -8,21 +25,61 @@ export const columnDef = [
     columnHelper.accessor('first_name', {
         header: 'First Name',
         id: 'first_name',
+        filterFn: myFn
     }),
     columnHelper.accessor('last_name', {
         header: 'Last Name',
-        id: 'last_name',
+        id: 'last_name'
     }),
     columnHelper.accessor('email', {
         header: 'Email',
-        id: 'email',
+        id: 'email'
+    }),
+    columnHelper.accessor('gender', {
+        header: (row: any) => {
+            // console.log('row: ', row);
+            return 'Gender';
+        },
+        id: 'gender'
+    } as any)
+    // columnHelper.accessor((row) => `${row.first_name} ${row.last_name}`, {
+    //     header: 'all in one',
+    // }),
+    // {
+    //     id: 'omg'
+    // }
+];
+
+export const columnDefWithGroup = [
+    columnHelper.group({
+        header: 'initials',
+        columns: [
+            columnHelper.accessor('first_name', {
+                header: 'First Name',
+                id: 'first_name'
+            }),
+            columnHelper.accessor('last_name', {
+                header: 'Last Name',
+                id: 'last_name'
+            })
+        ]
+    }),
+
+    columnHelper.accessor('email', {
+        header: 'Email',
+        id: 'email'
     }),
     columnHelper.accessor('gender', {
         header: 'Gender',
         id: 'gender',
         omg: 'omg'
     } as any),
-    // columnHelper.accessor((row) => `${row.first_name} ${row.last_name}`, {
-    //     header: 'all in one',
-    // }),
+    columnHelper.display({
+        header: 'display1',
+        id: 'display1',
+        cell: (info) => {
+            // console.log('info', info);
+            return 'test';
+        }
+    })
 ];
