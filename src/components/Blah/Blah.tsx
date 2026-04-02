edbe2e38d8b96887d67b745f3ecac01b1328399e
VictorSarov <victor.sarov@rentalmatics.com>
2026-04-02 10:56:19 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -5,42 +5,43 @@ import { Box, Skeleton, Typography } from '@mui/material';
 type Props = any;
 
 const Blah: React.FC<any> = (props: Props) => {
-    const countRef = React.useRef(0);
-    const htmlRef = React.useRef<HTMLDivElement>(null);
 
-    const [count, setCount] = React.useState(0);
+    const inputRefOne = React.useRef<any>(null);
+    const inputRefTwo = React.useRef<any>(null);
+    let name = 'Blah';
+    let value = 26;
 
     React.useEffect(() => {
-        if (htmlRef.current) {
-            htmlRef.current.style.color = 'red';
-        }
+        console.log('inputRefOne', inputRefOne);
+        console.log('inputRefTwo', inputRefTwo);
+        inputRefTwo.current.textContent = 'This is the second ref';
     }, []);
+
+    function handleClick(){
+        inputRefOne.current.focus();
+    }
 
     return (
         <div>
-            <h1>Count {countRef.current}</h1>
-            <h2>
-                count bounded: <span ref={htmlRef}>0</span>
-            </h2>
-            <button
-                onClick={() => {
-                    countRef.current = countRef.current + 1;
-                    if (htmlRef.current) {
-                        htmlRef.current.innerText = countRef.current.toString();
-                    }
-                }}
-            >
-                +
-            </button>
-            <button
-                onClick={() => {
-                    setCount(count + 1);
-                }}
-            >
-                re-render
-            </button>
+            <Child ref={{ ref1: inputRefOne, ref2: inputRefTwo }} {...{name, value, onChange: () => {return console.log('changed')}}} />
+            <button onClick={handleClick}>Focus Child Input</button>
         </div>
     );
 };
 
 export default Blah;
+
+
+const Child: React.FC<any> = React.forwardRef((props: Props, ref: any) => {
+
+    // console.log('ref', ref);
+    // console.log('props', props);
+
+    return (
+        <Box>
+            <Typography variant="h6">Child Component</Typography>
+            <input type="text" {...props} ref={ref?.ref1} />
+            <span ref={ref?.ref2} ></span>
+        </Box>
+    );
+})
