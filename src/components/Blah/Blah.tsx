26f5d90668491d40433c39017e1bfe535bf1ee6c
VictorSarov <victor.sarov@rentalmatics.com>
2026-04-02 09:49:33 +0100

    good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -4,12 +4,43 @@ import { Box, Skeleton, Typography } from '@mui/material';
 
 type Props = any;
 
-
 const Blah: React.FC<any> = (props: Props) => {
+    const countRef = React.useRef(0);
+    const htmlRef = React.useRef<HTMLDivElement>(null);
 
-    return <div>
-        <h1>Hello</h1>
-    </div>;
+    const [count, setCount] = React.useState(0);
+
+    React.useEffect(() => {
+        if (htmlRef.current) {
+            htmlRef.current.style.color = 'red';
+        }
+    }, []);
+
+    return (
+        <div>
+            <h1>Count {countRef.current}</h1>
+            <h2>
+                count bounded: <span ref={htmlRef}>0</span>
+            </h2>
+            <button
+                onClick={() => {
+                    countRef.current = countRef.current + 1;
+                    if (htmlRef.current) {
+                        htmlRef.current.innerText = countRef.current.toString();
+                    }
+                }}
+            >
+                +
+            </button>
+            <button
+                onClick={() => {
+                    setCount(count + 1);
+                }}
+            >
+                re-render
+            </button>
+        </div>
+    );
 };
 
 export default Blah;
