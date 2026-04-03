823a4fe15206b2cf730a03a401eae21440b4aed5
VictorSarov <victor.sarov@rentalmatics.com>
2026-04-03 13:57:34 +0100

    d

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -6,6 +6,16 @@ import Autocomplete from '@mui/material/Autocomplete';
 
 type Props = any;
 
+// const topFilms = [
+//    { name: "The Shawshank Redemption", year: 1994 },
+//    { name: "The Godfather", year: 1972 },
+//    { name: "The Godfather: Part II", year: 1974 },
+//    { name: "The Dark Knight", year: 2008 },
+//    { name: "12 Angry Men", year: 1957 },
+//    { name: "Schindler's List", year: 1993 },
+//    { name: "Pulp Fiction", year: 1994 },
+// ]
+
 const topFilms = [
     { label: 'The Shawshank Redemption', year: 1994 },
     { label: 'The Godfather', year: 1972 },
@@ -16,18 +26,16 @@ const topFilms = [
     { label: 'Pulp Fiction', year: 1994 }
 ];
 
-
-
 const Blah: React.FC<any> = (props: Props) => {
-
     const [inputValueState, setInputValueState] = React.useState('');
-    const [valueState, setValueState] = React.useState<any>(topFilms[3]);
+    const [valueState, setValueState] = React.useState<any>(null);
 
     React.useEffect(() => {
+        console.log('============================');
         console.log('inputValueState: ', inputValueState);
         console.log('valueState: ', valueState);
+        console.log('============================');
     }, [inputValueState, valueState]);
-
 
     return (
         <Box sx={{ m: 2, p: 2 }}>
@@ -39,7 +47,6 @@ const Blah: React.FC<any> = (props: Props) => {
 
                     return <TextField {...params} label='Movie' />;
                 }}
-
                 // states
                 inputValue={inputValueState}
                 onInputChange={(event, newInputValue) => {
@@ -49,8 +56,37 @@ const Blah: React.FC<any> = (props: Props) => {
                 onChange={(event, newValue) => {
                     setValueState(newValue);
                 }}
+                // custom labels
+                // getOptionLabel={(option) => {
+                //     console.log('option: ', option);
+                //     return option.name;
+                // }}
 
+                // filter options
+                filterOptions={(options, state) => {
+                    // console.log('options: ', options);
+                    // console.log('state: ', state);
 
+                    if (state.inputValue === 'omg') {
+                        return ['secret one', 'secret two'];
+                    }
+
+                    return topFilms;
+                }}
+                isOptionEqualToValue={(originalOptionItem: any, selectedValue: any) => {
+                    console.log('originalOptionItem: ', originalOptionItem);
+                    console.log('selectedValue: ', selectedValue);
+
+                    if (selectedValue === 'secret one' && originalOptionItem.label === topFilms[0].label) {
+                        return true;
+                    }
+
+                    if (selectedValue === 'secret two' && originalOptionItem.label === topFilms[1].label) {
+                        return true;
+                    }
+
+                    return originalOptionItem === selectedValue;
+                }}
                 // sx
                 sx={{ width: 400 }}
             />
