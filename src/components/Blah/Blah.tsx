b113174a84fe99f9a25706826b84720565de15cd
VictorSarov <victor.sarov@rentalmatics.com>
2026-05-22 13:19:34 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
-// @ts-ignore
+import ReactGA from "react-ga4";
 
 type Props = any;
+
+// G-8897TXN49S
+ReactGA.initialize("G-8897TXN49S");
 
 
 const Blah: React.FC<any> = (props: Props) => {
 
     return <div>
         <h1>Hello</h1>
+        <button onClick={() => ReactGA.event({ category: 'My Category', action: 'Clicked Button', label: 'Click me button' })}>
+            Click me
+        </button>
     </div>;
 };
 
