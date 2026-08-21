a57019db88d37dce4fc1bab4f75233a53aaf953d
VictorSarov <victor.sarov@rentalmatics.com>
2026-08-21 08:10:38 +0100

    all good

--- src/components/Blah/Blah.tsx
+++ src/components/Blah/Blah.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Box, Skeleton, Typography } from '@mui/material';
 // @ts-ignore
 
-
 type Props = any;
-
 
 // SX
 const containerSx: any = {
@@ -12,39 +10,69 @@ const containerSx: any = {
 
     display: 'flex',
 
-    flexDirection: 'row',
+    flexDirection: 'row'
     // justifyContent: 'right',
     // columnGap: '10px'
 
     // height: '90vh',
     // alignItems: 'center',
-    // margin: '10px'
-
-
-}
-
+    // margin: '10px',
+    // flexWrap: 'wrap',
+    // alignContent: 'end',
+    // alignItems: 'start',
+    // padding: '10px'
+};
 
 const boxSx: any = {
-    width: '150px',
-    height: '150px',
+    width: '250px',
+    // height: '150px',
     fontSize: '8rem',
     borderRadius: '15px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-}
-
-
-const Blah: React.FC<any> = (props: Props) => {
-
-
-    return <Box sx={containerSx}>
-        <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
-        <Box sx={{...boxSx, backgroundColor: 'yellow', order: 3}}>2</Box>
-        <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
-        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
-    </Box>;
+    border: '5px solid purple',
+    flex: 1,
+    height: '400px'
 };
 
+const smallBox = {
+    height: '150px',
+    flex: 1
+};
+
+const Blah: React.FC<any> = (props: Props) => {
+    return (
+        <>
+            <Box sx={{ border: '1px solid gray', padding: 2 }}>
+                <Box sx={{ width: '462px', gap: 1, display: 'flex' }}>
+                    <Box sx={{ ...smallBox, backgroundColor: 'red' }}>1</Box>
+                    <Box sx={{ ...smallBox, backgroundColor: 'yellow' }}>2</Box>
+                    <Box sx={{ ...smallBox, backgroundColor: 'green' }}>3</Box>
+                </Box>
+
+                <Box sx={{ mt: 1, gap: 1, display: 'flex' }}>
+                    <Box sx={{ ...boxSx, backgroundColor: 'red', flexGrow: 2 }}>1</Box>
+                    <Box sx={{ ...boxSx, backgroundColor: 'yellow', flexGrow: 2 }}>2</Box>
+                    <Box sx={{ ...boxSx, backgroundColor: 'green' }}>3</Box>
+                    <Box sx={{ ...boxSx, backgroundColor: 'blue' }}>4</Box>
+                </Box>
+
+                <Box sx={{ mt: 1, gap: 1, display: 'flex' }}>
+                    <Box sx={{ ...boxSx, backgroundColor: 'red', flexGrow: 1 }}>1</Box>
+                    <Box sx={{ ...boxSx, backgroundColor: 'yellow', flexGrow: 3 }}>2</Box>
+                    <Box sx={{ ...boxSx, backgroundColor: 'green', flexGrow: 2 }}>3</Box>
+
+                </Box>
+            </Box>
+
+            {/* <hr />
+            <Box sx={{ ...boxSx, backgroundColor: 'red' }}>1</Box>
+            <Box sx={{ ...boxSx, backgroundColor: 'yellow' }}>2</Box>
+            <Box sx={{ ...boxSx, backgroundColor: 'green' }}>3</Box>
+            <Box sx={{ ...boxSx, backgroundColor: 'blue' }}>4</Box> */}
+        </>
+    );
+};
 
 export default Blah;
